e9b57288683778325d975e47d03cb73b5a8eaf6d
LX-20160412CXLI\Administrator <449211097@qq.com>
2016-08-27 18:32:49 +0800

    22231313

--- Test/src/com/example/test/MainActivity.java
+++ Test/src/com/example/test/MainActivity.java
@@ -7,11 +7,10 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+	//111
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        
     }
 }
